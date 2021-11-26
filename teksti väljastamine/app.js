5ebb23cf8c87d70e89d677b727f25063205a9e2b
kristkas <92432962+kristkas@users.noreply.github.com>
2021-11-26 15:59:31 +0200

    teksti väljastamine

---
+++ teksti väljastamine/app.js
@@ -0,0 +1,30 @@
+// teksti väljastamine
+// kommentaaride kasutamine
+
+//üherealine kommentaar
+
+/*mitme
+realine
+kommentaar
+ */
+
+console.log("Tere Maailm ja teised")
+console.log("See on teine rida")
+
+console.log("See " + "on " + "liitlause ")
+console.log("Aastal 2016 valiti Eesti presidentiks Kersti Kaljulaid")
+
+
+//muutujad ja andmetüübid
+// 1. deklareerimine
+// let muutujanimi
+// 2. väärtuse omistamine
+// muutujanimi = väärtus
+
+let aasta
+aasta = 2016
+
+let president
+president= "Kersti Kaljulaid"
+
+console.log(aasta + " . Eesti presidendiks valiti " + president)
