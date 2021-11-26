941102eb81f169c2cc34f285e0eeac4c27009a80
kristkas <92432962+kristkas@users.noreply.github.com>
2021-11-26 20:17:19 +0200

    for tsükkel

---
+++ tsüklid/aratus.js
@@ -0,0 +1,6 @@
+// kasutajasisend
+let kordus = prompt("Mitu korda peab äratus käima?");
+// for tsökkel
+for(let kord = 1; kord <= kordus; kord++) {
+    console.log(kord + ". kord Tõuse ja sära");
+}
