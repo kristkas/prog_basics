39b975bad6cbecf4a9d53d201fba4b509cdc91d8
kristkas <92432962+kristkas@users.noreply.github.com>
2021-11-26 20:18:23 +0200

    while tsükkel

---
+++ tsüklid/janesed.js
@@ -0,0 +1,13 @@
+// kasutajasisend
+let ringid = prompt("Joostud ringide arv: ");
+let porgandid = 0;
+
+// while tsükkel
+let ring = 1;
+while(ring <= ringid) {
+    if(ring % 2 == 0) {
+        porgandid = porgandid + ring;
+    }
+    ring++;
+}
+console.log("Saadud porgandite arv on " + porgandid);
