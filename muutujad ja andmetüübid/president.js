eae3c48b3fda7470e6fc055e63162a51b5302863
kristkas <92432962+kristkas@users.noreply.github.com>
2021-11-26 17:45:58 +0200

    teksti väljastamine

---
+++ muutujad ja andmetüübid/president.js
@@ -0,0 +1,6 @@
+let aasta = 2006;
+let president = " Toomas Hendrik Ilves"
+let lause_keskosa = ". aastal valiti Eesti presidendiks"
+let lause = String(aasta) + lause_keskosa + president;
+
+console.log(lause)
